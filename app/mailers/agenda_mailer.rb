d2510dab491a9d0e82242d1e8a31f4c978347ae4
shin <5446shin@gmail.com>
2019-03-20 13:04:43 +0900

    fix: アジェンダを削除した時にTeamに所属しているユーザー全員に通知メールが飛ぶように修正

--- app/mailers/agenda_mailer.rb
+++ app/mailers/agenda_mailer.rb
@@ -1,7 +1,7 @@
 class AgendaMailer < ApplicationMailer
     def agenda_mail(agenda)
         @agenda = agenda
-        @members = @agenda.team.users
+        @members = @agenda.team.members
         mail to:@members.map(&:email).join(","), subject: "アジェンダ削除の通知メール"
     end
 end
